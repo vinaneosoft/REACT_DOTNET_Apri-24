0a2a1aafa99b4b015aed910d3f62d941cabade71
kokanevina <kokane.vina@gmail.com>
2024-04-02 17:52:49 +0530

    commit

---
+++ React Workspace/customercrudproject/src/App.js
@@ -0,0 +1,15 @@
+import { Header } from "./header/Header";
+
+
+
+function App() {
+  return (
+    <>
+      <h2>ONLINE FOOD ORDER</h2>
+      <Header></Header>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ React Workspace/customercrudproject/src/header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+export function Header(){
+    return (
+        <header className="myHeader">
+            <h3>CUSTOMER CRUD</h3>
+        </header>
+    );
+}
