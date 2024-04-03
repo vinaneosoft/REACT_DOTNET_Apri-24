31d0ca93bd748ac2196e85de71a162dee3c83806
kokanevina <kokane.vina@gmail.com>
2024-04-03 17:06:24 +0530

    commit

--- React Workspace/customercrudproject/src/header/Header.js
+++ React Workspace/customercrudproject/src/header/Header.js
@@ -1,12 +1,21 @@
 import { Navbar } from "../navbar/Navbar";
 import "./Header.css"
 
+
+
+
+
 export function Header(){
     let myHeading="CUSTOMER CRUP PROJECT";
+
+    function getSubheading(){
+        return "FOOD ORDER"
+    }
 
     return (
         <header className="myHeader">
             <h4>{myHeading}</h4>
+            <h5>{getSubheading()}</h5>
             <Navbar></Navbar>
         </header>
     );
