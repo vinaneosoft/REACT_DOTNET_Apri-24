ba1ebf36673018bac5b6684aef870f49dbc723de
kokanevina <kokane.vina@gmail.com>
2024-04-03 17:26:47 +0530

    commit

--- React Workspace/customercrudproject/src/test/Test.js
+++ React Workspace/customercrudproject/src/test/Test.js
@@ -1,13 +1,26 @@
 
 
 export function Test(){
-    let orderType="i";
+    let orderType="a";
+
+    let imageObject={
+        imagePath:"https://th.bing.com/th/id/OIP.BJZuAZfQRH9Cv7vDEyJ1dQHaE7?rs=1&pid=ImgDetMain",
+        altMessage:"food",
+        height:"200",
+        width:"400"
+    }
+
     return (
-        <ol type={orderType}>
-            <li>Veg Kadhai</li>
-            <li>Veg Kadhai</li>
-            <li>Veg Kadhai</li>
-            <li>Veg Kadhai</li>
-        </ol>
+        <>
+            <ol type={orderType}>
+                <li>Veg Kadhai</li>
+                <li>Veg Kadhai</li>
+                <li>Veg Kadhai</li>
+                <li>Veg Kadhai</li>
+            </ol>
+            <figure>
+                <img src={imageObject.imagePath} alt={imageObject.altMessage} height={imageObject.height} width={imageObject.width}></img>
+            </figure>
+        </>
     );
 }
