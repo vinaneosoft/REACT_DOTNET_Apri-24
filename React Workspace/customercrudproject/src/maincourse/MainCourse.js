17cb22be3f61749b97e68114e4e242632fddd871
vina <vinaneosoft@gmail.com>
2024-04-04 17:51:57 +0530

    commit

--- React Workspace/customercrudproject/src/maincourse/MainCourse.js
+++ React Workspace/customercrudproject/src/maincourse/MainCourse.js
@@ -1,21 +1,28 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { NonVegFood } from "../nonvegfood/NonVegFood";
 import { VegFood } from "../vegfood/VegFood";
 
 
 export function MainCourse(){
-    let foodType="nonveg"; // in future we will use logic of checkboxes here
+    let foodType=""; // in future we will use logic of checkboxes here
     let component;
-    if(foodType=="veg")
-        component=<VegFood></VegFood>
-    else
-        component=<NonVegFood></NonVegFood>
 
+    if(foodType==undefined || foodType == "")
+    component=<><VegFood></VegFood><NonVegFood></NonVegFood></>
+    else{
+        if(foodType=="veg")
+            component=<VegFood></VegFood>
+        else
+            component=<NonVegFood></NonVegFood>
+    }
     return (
         <section>
             <h4>Main Course</h4>
             {component}
             <h4>Main Course</h4>
             {foodType=="veg" ? <VegFood></VegFood>  : <NonVegFood></NonVegFood> }
+            <h4>Main Course 3</h4>
+            {(foodType==undefined || foodType=="") && <><VegFood></VegFood><NonVegFood></NonVegFood></>}
         </section>
     );
 }
