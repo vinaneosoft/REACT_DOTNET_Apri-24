6550bc4da87add3ab517a2d7c8c2a893e044b6fc
vina <vinaneosoft@gmail.com>
2024-04-12 16:27:04 +0530

    commit

--- React Workspace/customercrudproject/src/register/Register.js
+++ React Workspace/customercrudproject/src/register/Register.js
@@ -5,15 +5,31 @@ export function Register(){
         <section>
             <h5>CUSTOMER INPUT FORM</h5>
             <form>
-                <div class="mb-3">
-                    <label for="id" className="form-label">ID:</label>
+                <div className="mb-3">
+                    <label htmlFor="id" className="form-label">ID:</label>
                     <input type="text" className="form-control" id="id"></input>
                 </div>
-                <div class="mb-3">
-                    <label for="customerName" className="form-label">NAME:</label>
+                <div className="mb-3">
+                    <label htmlFor="customerName" className="form-label">NAME:</label>
                     <input type="text" className="form-control" id="customerName"></input>
                 </div>
-                <input type="submit" class="btn btn-primary" defaultValue="REGISTER"></input>
+                <div className="mb-3">
+                    <label htmlFor="customerContact" className="form-label">Mobile Number:</label>
+                    <input type="text" className="form-control" id="customerContact"></input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="customerEmail" className="form-label">Email Id:</label>
+                    <input type="text" className="form-control" id="customerEmail"></input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Set Username:</label>
+                    <input type="text" className="form-control" id="username"></input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Set Password:</label>
+                    <input type="text" className="form-control" id="password"></input>
+                </div>
+                <input type="submit" className="btn btn-primary" defaultValue="REGISTER"></input>
             </form>
         </section>
     );
