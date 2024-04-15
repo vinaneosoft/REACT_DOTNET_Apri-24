c82386c82124e6d8488710d94a52e69b5bf24b64
vina <vinaneosoft@gmail.com>
2024-04-15 16:14:37 +0530

    commit

--- React Workspace/customercrudproject/src/maincourse/MainCourse.js
+++ React Workspace/customercrudproject/src/maincourse/MainCourse.js
@@ -1,27 +1,28 @@
 
+import { useState } from "react";
 import { NonVegFood } from "../nonvegfood/NonVegFood";
 import { VegFood } from "../vegfood/VegFood";
 
 
 export function MainCourse(){
-    let foodType=""; // in future we will use logic of checkboxes here
-    let component;
-    if(foodType===undefined || foodType === "")
-    component=<><VegFood></VegFood><NonVegFood></NonVegFood></>
-    else{
-        if(foodType==="veg")
-            component=<VegFood></VegFood>
-        else
-            component=<NonVegFood></NonVegFood>
+    let [foodType, setType]= useState(""); // in future we will use logic of checkboxes here
+
+    function changeType(e){
+       // console.log(e.target.value);
+        setType(e.target.value);
     }
+    
     return (
         <section>
             <h4>Main Course</h4>
-            {component}
-            <h4>Main Course</h4>
-            {foodType==="veg" ? <VegFood></VegFood>  : <NonVegFood></NonVegFood> }
-            <h4>Main Course 3</h4>
-            {(foodType===undefined || foodType==="") && <><VegFood></VegFood><NonVegFood></NonVegFood></>}
+            <b>Select Option</b>
+            <input type="radio" name="ftype" value="vegfood" onChange={changeType}></input> <i>:VEG</i>
+            <input type="radio" name="ftype"  value="nonvegfood" onChange={changeType}></input><i> :NON VEG</i>
+            <div>
+                {foodType==="" && <><VegFood></VegFood><NonVegFood></NonVegFood></>}
+                {foodType==="vegfood" && <VegFood></VegFood>}
+                {foodType==="nonvegfood" && <NonVegFood></NonVegFood>}
+            </div>
         </section>
     );
 }
