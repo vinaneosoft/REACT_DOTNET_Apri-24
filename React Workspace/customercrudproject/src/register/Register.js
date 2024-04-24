a8b665a1e1bbe4921c4b2888836c64b15140ec18
vina <vinaneosoft@gmail.com>
2024-04-24 17:09:39 +0530

    commit

--- React Workspace/customercrudproject/src/register/Register.js
+++ React Workspace/customercrudproject/src/register/Register.js
@@ -1,3 +1,7 @@
+/** if we again on register then form must set to default customer again */
+
+
+
 import { useEffect, useState } from "react";
 import { Customer } from "../Model/Customer";
 import { addCustmer, searchCustomerById } from "../Model/CustomerCrud";
@@ -8,7 +12,7 @@ export function Register(){
     // take function red from useNavigate
     const navigate=useNavigate();
     // extract parameters from route : hook : useParams
-    let paramObject= useParams(); // 3. extracting data
+    //let paramObject= useParams(); // 3. extracting data
    // console.log(paramObject);
     //console.log(paramObject.cid);
     // object destructuring
@@ -17,13 +21,17 @@ export function Register(){
     //console.log(uname);
     // in future in react http, we are going to search a customer at backend having id extracted from route
     let [customer, setCustomer]=useState(new Customer());
+    let [heading, setHeading]=useState("REGISTER");
+    let [readonly, setReadOnly]=useState(false);
      /** customer : current state of Customer type object */
     async function search(){
         const cust=await searchCustomerById(cid);
         setCustomer(cust);
+        setHeading("UPDATE");
+        setReadOnly(true);
     }
     useEffect(()=>{
-        if(cid!=undefined){
+        if(cid!==undefined){
             search();
         }
     },[]);
@@ -47,11 +55,11 @@ export function Register(){
 
     return (
         <section>
-            <h5>CUSTOMER REGISTER / UPDATE FORM</h5>
-            <form onSubmit={collectData}>
+            <h5>CUSTOMER {heading} FORM</h5>
+            <form onSubmit={collectData} className="bg-warning p-2">
                 <div className="mb-3">
                     <label htmlFor="id" className="form-label">ID:</label>
-                    <input type="text" className="form-control" id="id" name="id" value={customer.id} onChange={handleEvent}></input>
+                    <input type="text" className="form-control" id="id" name="id" value={customer.id} onChange={handleEvent} readOnly={readonly}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="customerName" className="form-label">NAME:</label>
@@ -67,13 +75,13 @@ export function Register(){
                 </div>
                 <div className="mb-3">
                     <label htmlFor="username" className="form-label">Set Username:</label>
-                    <input type="text" className="form-control" id="username" name="username"  value={customer.username} onChange={handleEvent}></input>
+                    <input type="text" className="form-control" id="username" name="username"  value={customer.username} onChange={handleEvent} readOnly={readonly}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Set Password:</label>
-                    <input type="password" className="form-control" id="password" name="password"  value={customer.password} onChange={handleEvent}></input>
+                    <input type="password" className="form-control" id="password" name="password"  value={customer.password} onChange={handleEvent} readOnly={readonly}></input>
                 </div>
-                <input type="submit" className="btn btn-primary" defaultValue="REGISTER"></input>
+                <input type="submit" className="btn btn-primary" value={heading}  onChange={handleEvent}></input>
             </form>
             <p>
                 <i>Already a user??? click <Link to="/login">here</Link> to login!!!!</i>
