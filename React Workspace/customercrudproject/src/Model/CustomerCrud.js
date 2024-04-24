13502238ccfb493a33971ef7652c9024a54640af
vina <vinaneosoft@gmail.com>
2024-04-24 15:46:26 +0530

    commit

--- React Workspace/customercrudproject/src/Model/CustomerCrud.js
+++ React Workspace/customercrudproject/src/Model/CustomerCrud.js
@@ -7,7 +7,7 @@ const url="http://localhost:3001/customers";
 export async function addCustmer(customer){
      // connect to backend to add in data in json file
    // console.log(customer);
-    const response=await axios.post(url,customer);
+    const response=await axios.post(url,customer); /** data sent from request body */
    // console.log(response);
    return response.status;  
 }
@@ -18,5 +18,6 @@ export async function getCustomers(){
 }
 
 export async function deleteCutomerById(id){
-   
+   const response=await axios.delete(`${url}/${id}`); // or url+"/"+id
+   console.log(response);
 }
